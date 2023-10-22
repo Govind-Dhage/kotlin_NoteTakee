166d00e5329dbb440c14c70f16bd9d80dcbafde2
gdtechnical <govinddhage@gmail.com>
2023-10-23 00:06:59 +0530

    First Commit

---
+++ app/src/main/java/com/govind/dhage/kotlin_notetakee/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.govind.dhage.kotlin_notetakee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
